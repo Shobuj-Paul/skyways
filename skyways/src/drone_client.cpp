692bf9893ad7fa31a083aa5d0e9669f7ebd36da4
Shobuj Paul <shobuj.paul@gmail.com>
2022-06-28 08:41:03 +0530

    added more arguments to drone client

--- skyways/src/drone_client.cpp
+++ skyways/src/drone_client.cpp
@@ -8,17 +8,29 @@ using namespace std::chrono_literals;
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  if(argc!=2) {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Usage: drone_client <vehicle_id>");
+  if(argc!=8) {
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Usage: drone_client <vehicle_id> <start x> <start y> <start z> <end x> <end y> <end z>");
     return 1;
   }
   std::string id = argv[1]; 
+  double start_x = atof(argv[2]);
+  double start_y = atof(argv[3]);
+  double start_z = atof(argv[4]);
+  double end_x = atof(argv[5]);
+  double end_y = atof(argv[6]);
+  double end_z = atof(argv[7]);
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("drone_client");
   rclcpp::Client<skyways::srv::DataPacket>::SharedPtr client = node->create_client<skyways::srv::DataPacket>("drone_service");
   rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr publisher = node->create_publisher<geometry_msgs::msg::PoseArray>(id + "/waypoints_publisher", 10);
   rclcpp::WallRate loop_rate(500ms);
   auto request = std::make_shared<skyways::srv::DataPacket::Request>();
   request->vehicle_id = id;
+  request->start_position.x = start_x;
+  request->start_position.y = start_y;
+  request->start_position.z = start_z;
+  request->end_position.x = end_x;
+  request->end_position.y = end_y;
+  request->end_position.z = end_z;
   RCLCPP_INFO(rclcpp::get_logger("sending"), "Vehicle ID: %s", request->vehicle_id.c_str());
   RCLCPP_INFO(rclcpp::get_logger("sending"), "Start Position: %f %f %f", request->start_position.x, request->start_position.y, request->start_position.z);
   RCLCPP_INFO(rclcpp::get_logger("sending"), "End Position: %f %f %f", request->end_position.x, request->end_position.y, request->end_position.z);
