aef8293dfeb43aaf9f4e6131ca223431b5653258
Shobuj Paul <shobuj.paul@gmail.com>
2022-07-16 22:08:00 +0530

    removed hard coding

--- skyways/src/drone_client.cpp
+++ skyways/src/drone_client.cpp
@@ -1,6 +1,5 @@
 #include <rclcpp/rclcpp.hpp>
 #include <skyways/srv/data_packet.hpp>
-#include <geometry_msgs/msg/pose_array.hpp>
 #include <chrono>
 
 using namespace std::chrono_literals;
@@ -8,40 +7,40 @@ using namespace std::chrono_literals;
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv); //Initialize the node
-  if(argc!=8) //Check condition for minimum number of arguments
+  if(argc!=2) //Check condition for minimum number of arguments
   {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Usage: drone_client <vehicle_id> <start x> <start y> <start z> <end x> <end y> <end z>");
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Usage: drone_client <vehicle_id>");
     return 1;
   }
   // Storing the command line arguments into temporary variables
-  std::string id = argv[1]; 
-  double start_x = atof(argv[2]);
-  double start_y = atof(argv[3]);
-  double start_z = atof(argv[4]);
-  double end_x = atof(argv[5]);
-  double end_y = atof(argv[6]);
-  double end_z = atof(argv[7]);
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("drone_client"); //Create a node object.
+  std::string id = argv[1]; //Vehicle ID
+  
+  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("drone_client"); //Create a node object.
   rclcpp::Client<skyways::srv::DataPacket>::SharedPtr client = node->create_client<skyways::srv::DataPacket>("drone_service"); //Create the client object.
-  rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr publisher = node->create_publisher<geometry_msgs::msg::PoseArray>(id + "/waypoints_publisher", 10); //Create a publisher object with specific vehicle ID.
+  rclcpp::Publisher<skyways::srv::DataPacket_Response>::SharedPtr publisher = node->create_publisher<skyways::srv::DataPacket_Response>(id + "/data_packet", 10); //Create a publisher object with specific vehicle ID.
   rclcpp::WallRate loop_rate(500ms); //Rate object for sleep time in the loop.
+  
   auto request = std::make_shared<skyways::srv::DataPacket::Request>(); //Request object for accessing request data packet.
+  
   //Assign request data packet values.
   request->vehicle_id = id;
-  request->start_position.x = start_x;
-  request->start_position.y = start_y;
-  request->start_position.z = start_z;
-  request->end_position.x = end_x;
-  request->end_position.y = end_y;
-  request->end_position.z = end_z;
+  request->start_position.x = 0;
+  request->start_position.y = 0;
+  request->start_position.z = 0;
+  request->end_position.x = 0;
+  request->end_position.y = 0;
+  request->end_position.z = 0;
+  
   //Log the values in terminal.
   RCLCPP_INFO(rclcpp::get_logger("sending"), "Vehicle ID: %s", request->vehicle_id.c_str());
-  RCLCPP_INFO(rclcpp::get_logger("sending"), "Start Position: %f %f %f", request->start_position.x, request->start_position.y, request->start_position.z);
-  RCLCPP_INFO(rclcpp::get_logger("sending"), "End Position: %f %f %f", request->end_position.x, request->end_position.y, request->end_position.z);
+  //RCLCPP_INFO(rclcpp::get_logger("sending"), "Start Position: %f %f %f", request->start_position.x, request->start_position.y, request->start_position.z);
+  //RCLCPP_INFO(rclcpp::get_logger("sending"), "End Position: %f %f %f", request->end_position.x, request->end_position.y, request->end_position.z);
 
   //Wait for the service to be available.
-  while (!client->wait_for_service(1s)) {
-    if (!rclcpp::ok()) {
+  while (!client->wait_for_service(1s)) 
+  {
+    if (!rclcpp::ok())
+    {
       RCLCPP_ERROR(rclcpp::get_logger("client"), "Interrupted while waiting for the service. Exiting.");
       return 0;
     }
@@ -49,37 +48,43 @@ int main(int argc, char **argv)
   }
 
   auto result = client->async_send_request(request);
-  geometry_msgs::msg::PoseArray waypoints;
+  skyways::srv::DataPacket_Response packet;
   // Wait for the result.
   if (rclcpp::spin_until_future_complete(node, result) == rclcpp::FutureReturnCode::SUCCESS)
   {
-      waypoints = result.get()->waypoints; //Get the waypoints from the response.
-      RCLCPP_INFO(rclcpp::get_logger("recieved"), "Lane ID: %d", result.get()->lane_id);
+      packet.waypoints = result.get()->waypoints; //Get the waypoints from the response.
+      packet.vel_mag = result.get()->vel_mag; //Get the velocity magnitude from the response.
+      packet.geofence_radius = result.get()->geofence_radius; //Get the geofence radius from the response.
+      packet.corridor_radius = result.get()->corridor_radius; //Get the corridor radius from the response.
+      packet.lane_id = result.get()->lane_id; //Get the lane ID from the response.
+      packet.altitude = result.get()->altitude; //Get the altitude from the response.
       RCLCPP_INFO(rclcpp::get_logger("recieved"), "Velocity Magnitude: %f", result.get()->vel_mag);
       RCLCPP_INFO(rclcpp::get_logger("recieved"), "Geofence Radius: %f", result.get()->geofence_radius);
       RCLCPP_INFO(rclcpp::get_logger("recieved"), "Corridor Radius: %f", result.get()->corridor_radius);
-      auto n = waypoints.poses.size(); //Get the number of waypoints.
+      RCLCPP_INFO(rclcpp::get_logger("recieved"), "Altitude: %d", result.get()->altitude);
+      auto n = packet.waypoints.poses.size(); //Get the number of waypoints.
       for(int i=0; i<(int)n; i++) //Log the waypoints in the terminal
       {
-        RCLCPP_INFO(rclcpp::get_logger("received"), "Waypoint %d: %f %f %f", i, waypoints.poses[i].position.x, waypoints.poses[i].position.y, waypoints.poses[i].position.z);
+        RCLCPP_INFO(rclcpp::get_logger("received"), "Waypoint %d: %f %f %f", i, packet.waypoints.poses[i].position.x, packet.waypoints.poses[i].position.y, packet.waypoints.poses[i].position.z);
       }
-  } else 
+  } 
+  else 
   {
     RCLCPP_ERROR(rclcpp::get_logger("client"), "Failed to call service drone_service.");
   }
 
-  for(int i=0; i<100 && rclcpp::ok(); i++) //Start publisher to publish waypoints for ROS 1 control node.
+  while(rclcpp::ok()) //Start publisher to publish waypoints for ROS 1 control node.
   {
     RCLCPP_INFO(rclcpp::get_logger("publisher"), "Publishing waypoints on topic waypoints_publisher");
     try {
-      publisher->publish(waypoints);
+      publisher->publish(packet);
       rclcpp::spin_some(node);
-    } catch (const std::exception & e) {
+    } 
+    catch (const std::exception & e) {
       RCLCPP_ERROR(rclcpp::get_logger("publisher"), "Exception: %s", e.what());
     }
     loop_rate.sleep(); //Sleep for some time.
   }
-
   rclcpp::shutdown(); //Shutdown the node.
   return 0;
 }
--- skyways/src/drone_server.cpp
+++ skyways/src/drone_server.cpp
@@ -5,51 +5,19 @@
 void data(const std::shared_ptr<skyways::srv::DataPacket::Request> request, std::shared_ptr<skyways::srv::DataPacket::Response> response)
 {
   if (request->vehicle_id == "drone1") {
-    response->lane_id = 1;
     response->vel_mag = 1.0;
-    response->geofence_radius = 2.5;
+    response->geofence_radius = 1.0;
     response->corridor_radius = 5.0;
-    response->waypoints.poses.resize(5);
-    response->waypoints.poses[0].position.x = 0;
-    response->waypoints.poses[0].position.y = 0;
-    response->waypoints.poses[0].position.z = 5;
-    response->waypoints.poses[1].position.x = 0;
-    response->waypoints.poses[1].position.y = 5;
-    response->waypoints.poses[1].position.z = 5;
-    response->waypoints.poses[2].position.x = 5;
-    response->waypoints.poses[2].position.y = 5;
-    response->waypoints.poses[2].position.z = 5;
-    response->waypoints.poses[3].position.x = 5;
-    response->waypoints.poses[3].position.y = 0;
-    response->waypoints.poses[3].position.z = 5;
-    response->waypoints.poses[4].position.x = 0;
-    response->waypoints.poses[4].position.y = 0;
-    response->waypoints.poses[4].position.z = 5;
+    response->altitude = 5.0;
   }
   else if (request->vehicle_id == "drone2") {
-    response->lane_id = 1;
     response->vel_mag = 1.0;
-    response->geofence_radius = 2.5;
+    response->geofence_radius = 1.0;
     response->corridor_radius = 5.0;
-    response->waypoints.poses.resize(5);
-    response->waypoints.poses[0].position.x = 0;
-    response->waypoints.poses[0].position.y = 0;
-    response->waypoints.poses[0].position.z = 5;
-    response->waypoints.poses[1].position.x = 5;
-    response->waypoints.poses[1].position.y = 0;
-    response->waypoints.poses[1].position.z = 5;
-    response->waypoints.poses[2].position.x = 5;
-    response->waypoints.poses[2].position.y = 5;
-    response->waypoints.poses[2].position.z = 5;
-    response->waypoints.poses[3].position.x = 0;
-    response->waypoints.poses[3].position.y = 5;
-    response->waypoints.poses[3].position.z = 5;
-    response->waypoints.poses[4].position.x = 0;
-    response->waypoints.poses[4].position.y = 0;
-    response->waypoints.poses[4].position.z = 5;
+    response->altitude = 5.0;
   }
   else {
-      RCLCPP_ERROR(rclcpp::get_logger("client"), "Vehicle ID not found.");
+    RCLCPP_ERROR(rclcpp::get_logger("client"), "Vehicle ID not found.");
   }
 
   RCLCPP_INFO(rclcpp::get_logger("received"), "Vehicle ID: %s", request->vehicle_id.c_str());
@@ -60,12 +28,8 @@ void data(const std::shared_ptr<skyways::srv::DataPacket::Request> request, std:
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv); //Initialize the ROS2 node.
-
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("drone_server"); //Create node object.
-
-  rclcpp::Service<skyways::srv::DataPacket>::SharedPtr service =             
-    node->create_service<skyways::srv::DataPacket>("drone_service",  &data); // Create service object with callback function.
-
+  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("drone_server"); //Create node object.
+  rclcpp::Service<skyways::srv::DataPacket>::SharedPtr service = node->create_service<skyways::srv::DataPacket>("drone_service",  &data); // Create service object with callback function.
   RCLCPP_INFO(rclcpp::get_logger("drone_server"), "Ready to send data packet.");
 
   rclcpp::spin(node); //Spin the node once.
