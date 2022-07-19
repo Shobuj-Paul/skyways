f120e65bb721a7ee6b8c0148cab994b698b520b0
Shobuj Paul <shobuj.paul@gmail.com>
2022-07-19 08:43:48 +0530

    added node for gfswitch

---
+++ skyways/src/switch.cpp
@@ -0,0 +1,32 @@
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/bool.hpp>
+
+using namespace std::chrono_literals;
+
+class SwitchPublisher : public rclcpp::Node
+{
+    private:
+        rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr publisher_;
+        rclcpp::TimerBase::SharedPtr timer_;
+        size_t count;
+    public:
+        SwitchPublisher() : Node("switch_publisher")
+        {
+            publisher_ = this->create_publisher<std_msgs::msg::Bool>("/gfswitch", 10);
+            auto timer_callback = [this]()-> void {
+                auto message = std_msgs::msg::Bool();
+                message.data = true;
+                RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data ? "true" : "false");
+                this->publisher_->publish(message);
+            };
+            timer_ = this->create_wall_timer(500ms, timer_callback);
+        }
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<SwitchPublisher>());
+    rclcpp::shutdown();
+    return 0;
+}
