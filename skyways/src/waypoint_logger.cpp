8ea629660bbeb7603fb00a95e4af101a453958d8
Shobuj Paul <shobuj.paul@gmail.com>
2022-07-20 21:38:11 +0530

    fixed waypoint logger

--- skyways/src/waypoint_logger.cpp
+++ skyways/src/waypoint_logger.cpp
@@ -3,28 +3,23 @@
 #include <fstream>
 
 using namespace std;
-using std::placeholders::_1;
 
 class NavSatSubscriber : public rclcpp::Node
 {
-    public:
-        NavSatSubscriber(string id) : Node("gps_subscriber")
-        {
-            subscription_ =  this->create_subscription<sensor_msgs::msg::NavSatFix>(id + "/mavros/global_position/global", 10, [this](const sensor_msgs::msg::NavSatFix::UniquePtr msg)
-            {
-                ofstream WaypointLogger("src/skyways/text_files/WaypointLog.txt");
-                    char ch = 'y';
-                    while(ch!='n')
-                    {
-                        WaypointLogger << msg->latitude << endl << msg->longitude << endl;
-                        cout<<"Log another waypoint (y/n)? ";
-                        cin>>ch;
-                    }
-                    WaypointLogger.close();
-            });
-        }
     private:
         rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr subscription_;
+        void gps_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg)
+            {
+                lat = msg->latitude;
+                lon = msg->longitude;
+                rclcpp::shutdown();
+            }
+    public:
+        double lat, lon;
+        NavSatSubscriber(string id) : Node("gps_subscriber")
+        {
+            subscription_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(id + "/mavros/global_position/global", rclcpp::SensorDataQoS(), std::bind(&NavSatSubscriber::gps_callback, this, std::placeholders::_1));
+        }
 };
 
 int main(int argc, char * argv[])
@@ -35,8 +30,14 @@ int main(int argc, char * argv[])
         RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Usage: waypoint_logger <vehicle_id>");
         return 1;
     }
-    string id = argv[1];
-    rclcpp::spin(std::make_shared<NavSatSubscriber>(id));
-    rclcpp::shutdown();
+    string id = string(argv[1]);
+    auto node = std::make_shared<NavSatSubscriber>(id);
+    rclcpp::spin(node);
+    RCLCPP_INFO(rclcpp::get_logger("check"), "Latitude: %lf Longitude: %lf", node.get()->lat, node.get()->lon);
+    
+    ofstream WaypointLogger("src/skyways/text_files/WaypointLog.txt", ios_base::app);
+    WaypointLogger << node.get()->lat << " " << node.get()->lon << endl;
+    WaypointLogger.close();
+
     return 0;
 }
